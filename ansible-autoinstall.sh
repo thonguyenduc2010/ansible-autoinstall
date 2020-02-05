3c3c8964fc1d3459740409910eb3fbb374baa420
Nguyễn Đức Thọ <47622385+thonguyenduc2010@users.noreply.github.com>
2020-02-05 15:01:37 +0700

    Update ansible-autoinstall.sh

--- ansible-autoinstall.sh
+++ ansible-autoinstall.sh
@@ -9,4 +9,12 @@ sudo apt-get upgrade -y
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible -y
+
+echo "Install Python"
 sudo apt-get install python -y
+
+#Install Pip3
+echo "Install Pip3"
+sudo apt install python3-pip
+
+echo pip3 --version
